9347218ba43424dbae34df959a6f8de09eab97f9
Weiji Zhuang <zhuangweiji@foxmail.com>
2020-03-24 21:49:43 +0800

    support plot kaldi scp feats

---
+++ local/plot_feats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Copyright 2020 Xiaomi Corporation (author: Weiji Zhuang)
+# 2020/3/24
+
+# Apache 2.0
+
+
+import kaldi_io
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+
+def plot_feats(feats, note, file_name):
+    """Draw the spectrogram picture
+        :param spec: a feature_dim by num_frames array(real)
+        :param note: title of the picture
+        :param file_name: name of the file
+    """
+    fig = plt.figure(figsize=(20, 5))
+    heatmap = plt.pcolor(feats)
+    fig.colorbar(mappable=heatmap)
+    plt.xlabel('Time(s)')
+    plt.ylabel(note)
+    plt.tight_layout()
+    plt.savefig(file_name)
+
+
+def read_all_data(feat_scp):
+    feat_fid = open(feat_scp, 'r')
+    feat = feat_fid.readlines()
+    feat_fid.close()
+    id_list = []
+    mat_list = []
+
+    for i in range(len(feat)):
+        id, ark = feat[i].split()
+        mat = kaldi_io.read_mat(ark)
+        id_list.append(id)
+        mat_list.append(mat)
+    return id_list, mat_list
+
+
+def main():
+    ids, feats = read_all_data('/home/storage04/zhuangweiji/workspace/x260remotespace/DAE/feats.scp')
+    for i in range(len(ids)):
+        plot_feats(feats[i].T, ids[i],
+                   '/home/storage04/zhuangweiji/workspace/x260remotespace/DAE/'+ ids[i] +'.png')
+
+if __name__ == '__main__':
+    main()
